b0637bc221c766efe760a9b5a80f6b6254a1c5c5
xznikos <nikolasmota2@gmail.com>
2025-06-25 13:19:45 -0300

    Delete dev2/padaria_nivus directory

--- dev2/padaria_nivus/encomendas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EncomendasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'encomendas'
--- dev2/padaria_nivus/encomendas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import EncomendasModel
-
-class EncomendasForm(forms.ModelForm):
-    class Meta:
-        model = EncomendasModel
-        fields = ['nome', 'preco', 'imagem']
