c2446b10213d8e2436fa4c744088d695ac824a28
xznikos <nikolasmota2@gmail.com>
2025-11-23 16:05:34 -0300

    Adiciona arquivos para deploy no Render

---
+++ dev2/padaria_nivus/build.sh
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env bash
+set -o errexit
+
+echo "=== Instalando dependências ==="
+pip install -r requirements.txt
+
+echo "=== Coletando arquivos estáticos ==="
+python manage.py collectstatic --noinput
+
+echo "=== Aplicando migrações do banco ==="
+python manage.py migrate
